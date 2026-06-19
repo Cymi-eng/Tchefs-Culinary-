0e9ca836baedbbfcaab3570bb55f6a851554ff73
cymi-eng <eaiancymi@gmail.com>
2026-06-19 03:05:55 +0300

    add content to courses.json

--- courses.js
+++ courses.js
@@ -1,55 +1,51 @@
-const coursesContainer = document.getElementById("coursesContainer");
+const coursesContainer =
+document.getElementById("coursesContainer");
 
 fetch("courses.json")
-  .then((response) => response.json())
-  .then((courses) => {
-    courses.forEach((course) => {
+  .then(response => response.json())
+  .then(courses => {
+
+    courses.forEach(course => {
+
       coursesContainer.innerHTML += `
-      
-      <div class="bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-2xl transition duration-300">
+
+      <div class="bg-white rounded-2xl shadow-lg overflow-hidden">
 
         <img
-          src="${course.image}"
-          alt="${course.title}"
-          class="w-full h-56 object-cover"
-        >
+        src="${course.image}"
+        alt="${course.title}"
+        class="w-full h-56 object-cover">
 
         <div class="p-6">
 
-          <h3 class="text-2xl font-bold text-primary">
+          <h3 class="text-xl font-bold text-primary">
             ${course.title}
           </h3>
 
-          <p class="text-gray-600 mt-3">
+          <p class="text-accent mt-2">
+            Duration: ${course.duration}
+          </p>
+
+          <p class="text-gray-600 mt-4">
             ${course.description}
           </p>
 
-          <div class="mt-6 flex justify-between items-center">
+          <button
+          class="mt-6 bg-primary text-white px-6 py-3 rounded-lg w-full">
 
-            <span class="bg-primary text-white px-4 py-2 rounded-full text-sm">
-              ${course.duration}
-            </span>
+          Enroll Now
 
-            <button
-              class="bg-accent text-white px-4 py-2 rounded-lg hover:bg-red-700 transition"
-            >
-              Enroll
-            </button>
-
-          </div>
+          </button>
 
         </div>
 
       </div>
 
       `;
+
     });
+
   })
-  .catch((error) => {
-    coursesContainer.innerHTML = `
-      <p class="text-red-500 text-center col-span-full">
-        Failed to load courses.
-      </p>
-    `;
-    console.error(error);
+  .catch(error => {
+    console.log(error);
   });
